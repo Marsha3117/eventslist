168077de165dbc8f22b4c34d56563f48bd548d97
Marsha3117 <majoramos218@gmail.com>
2025-04-24 16:04:50 -0500

    arreglado

--- events.js
+++ events.js
@@ -3,23 +3,44 @@ document.getElementById("agregar").addEventListener('click', function(){
     const tarea = input.value.trim();
 
     if(tarea){
-       const saludo = tarea;
-       document.getElementById("list").innerHTML = saludo;
+
+        const nuevoitem = document.createElement("li"); //creo una constante y creo los li
+        nuevoitem.textContent = tarea; // esa constante guarda lo que pongo en el input
+        document.getElementById("list").appendChild(nuevoitem); // Voy a la lista, la llamo po ID y Agrego el nuevo li a la lista ul
         input.value = ''; // borra el input
 
-        // botón eliminar
-        const botonEliminarTarea = document.createElement("button");
-        botonEliminarTarea.textContent = "Eliminar";
-        saludo.remove()
+        
+        const botonborrar = document.createElement("button"); //Crear el elemento botón - está creado en js pero no se muestra
+        botonborrar.textContent = 'Eliminar'; //Establecer el texto del botón
+        nuevoitem.appendChild(botonborrar); //Se muestra el Botón
+        //Agregar un evento listener para que el botón haga algo al ser clickeado
+        botonborrar.addEventListener('click', function() {
+            nuevoitem.remove()
+            botonborrar.remove()           
+        });
+
+        const botontachar = document.createElement("button");
+        botontachar.textContent = 'Completada';
+        nuevoitem.appendChild(botontachar);
+
+        botontachar.addEventListener('click', function() {
+            if (nuevoitem.style.textDecoration === 'line-through') {
+                nuevoitem.style.textDecoration = ''; // Quita el tachado
+            } else {
+                nuevoitem.style.textDecoration = 'line-through'; // Aplica el tachado
+            }
+        });
+
+
 
      } else {
          document.getElementById("saludo").innerHTML = "Debe escribir una tarea";
      }
  });
 
- 
-//  const botonEliminarItem = document.getElementById("boton-eliminar-item")
-// botonEliminarItem.addEventListener("click", function() {
-//   lista.lastElementChild.remove()
 
-// })
+    //botón eliminar lista
+const borrarlista = document.getElementById("borrarlista")
+borrarlista.addEventListener("click", function() {
+    document.querySelector("li").remove()
+})
